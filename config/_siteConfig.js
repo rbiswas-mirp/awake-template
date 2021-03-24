3e64a21b659886a2a34bd2b753df4f6b00562e30
rbiswas-mirp <biswasrahul016@gmail.com>
2021-03-24 08:50:56 +0530

    Update _siteConfig.js

--- config/_siteConfig.js
+++ config/_siteConfig.js
@@ -41,7 +41,7 @@ export default {
 
   // Disqus
   disqus: {
-    on: true,
+    on: false,
     loadingStrategy: 'button', // Options: onload, lazy, button
     siteShortName: 'rb-blog' // 'blog-danielkelly-io'
   },
